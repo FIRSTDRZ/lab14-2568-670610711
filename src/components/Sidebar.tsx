e0d2ddad44d1c539013183603d3fc74cfa956270
first <firstgang17@gmail.com>
2025-09-16 08:48:34 +0700

    edited02

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,10 +38,10 @@ export default function Sidebar() {
             color="red"
             withBorder
           >
-            <Avatar size="md" radius="xl" src="/charliebrown.jpg" />
+            <Avatar size="md" radius="xl" src="/picturegu.JPEG" />
           </Indicator>
           <Text component={RouterNavLink} to="/">
-            User : Chanadda : Admin
+            User : Nasapong : student
           </Text>
         </Group>
       </Box>
